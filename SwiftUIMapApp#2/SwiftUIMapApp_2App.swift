23bd3cb0c52d66c2d3f333debe06a8a87f05690a
dev_demir <dev_demir@outlook.com>
2023-02-18 00:27:32 +0300

    Initial Commit

---
+++ SwiftUIMapApp#2/SwiftUIMapApp_2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapApp_2App.swift
+//  SwiftUIMapApp#2
+//
+//  Created by Emrullah Demir on 18.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapApp_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
