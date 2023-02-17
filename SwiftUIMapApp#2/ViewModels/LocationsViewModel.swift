1f39b66d2b3f37f887dabc2c73f386e432a27d52
dev_demir <dev_demir@outlook.com>
2023-02-18 01:36:42 +0300

    Map added to screen 💪🏼

--- SwiftUIMapApp#2/ViewModels/LocationsViewModel.swift
+++ SwiftUIMapApp#2/ViewModels/LocationsViewModel.swift
@@ -6,14 +6,37 @@
 //
 
 import Foundation
+import MapKit
 
 class LocationsViewModel : ObservableObject {
     
+    // All locations
     @Published var locations : [Location]
+    
+    // Current map location
+    @Published var mapLocation : Location {
+        didSet{
+            setMapCoordination(location: mapLocation)
+        }
+    }
+    
+    // Empty coordinate region
+    @Published var mapCoordinateRegion : MKCoordinateRegion = MKCoordinateRegion()
+        
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init(){
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        
+        self.setMapCoordination(location: self.mapLocation)
+    }
+    
+    private func setMapCoordination(location :Location){
+        mapCoordinateRegion = MKCoordinateRegion(
+            center: location.coordinates,
+            span: mapSpan)
     }
     
 }
