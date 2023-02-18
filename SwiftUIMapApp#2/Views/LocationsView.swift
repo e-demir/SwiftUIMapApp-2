42e3d2a9fe2e8632c37be4f293a2166b22a52510
dev_demir <dev_demir@outlook.com>
2023-02-18 14:23:18 +0300

    Header added

--- SwiftUIMapApp#2/Views/LocationsView.swift
+++ SwiftUIMapApp#2/Views/LocationsView.swift
@@ -16,6 +16,14 @@ struct LocationsView: View {
         ZStack{
             Map(coordinateRegion: $vm.mapCoordinateRegion)
                 .ignoresSafeArea()
+            
+            VStack(spacing: 0){
+               header
+                .padding()
+                
+                Spacer()
+            }
+            
         }
     }
 }
@@ -24,5 +32,28 @@ struct ListView_Previews: PreviewProvider {
     static var previews: some View {
         LocationsView()
             .environmentObject(LocationsViewModel())
+    }
+}
+
+// VIEWS EXTENSION
+extension LocationsView{
+    private var header : some View{
+        VStack {
+            Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+                .font(.title2)
+                .fontWeight(.black)
+                .foregroundColor(.primary)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .overlay(alignment: .leading) {
+                    Image(systemName: "arrow.down")
+                        .font(.headline)
+                        .foregroundColor(.primary)
+                        .padding()
+                }
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10)
+        .shadow(color: Color.black, radius: 20, x: 0, y: 15)
     }
 }
